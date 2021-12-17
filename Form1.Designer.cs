76c2ad34b3f3fc544a9b91d0e9a2797577821096
pomanas <slawomir.pomana@gmail.com>
2021-12-17 17:17:11 +0100

    Commit_1

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Pictures
+{
+    partial class F_Picture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_select = new System.Windows.Forms.Button();
+            this.bt_Remove = new System.Windows.Forms.Button();
+            this.pb_Picture1 = new System.Windows.Forms.PictureBox();
+            this.ofd_picture = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Picture1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_select
+            // 
+            this.bt_select.Location = new System.Drawing.Point(61, 31);
+            this.bt_select.Name = "bt_select";
+            this.bt_select.Size = new System.Drawing.Size(75, 23);
+            this.bt_select.TabIndex = 0;
+            this.bt_select.Text = "Select";
+            this.bt_select.UseVisualStyleBackColor = true;
+            this.bt_select.Click += new System.EventHandler(this.bt_select_Click);
+            // 
+            // bt_Remove
+            // 
+            this.bt_Remove.Enabled = false;
+            this.bt_Remove.Location = new System.Drawing.Point(226, 31);
+            this.bt_Remove.Name = "bt_Remove";
+            this.bt_Remove.Size = new System.Drawing.Size(75, 23);
+            this.bt_Remove.TabIndex = 1;
+            this.bt_Remove.Text = "Remove";
+            this.bt_Remove.UseVisualStyleBackColor = true;
+            this.bt_Remove.Click += new System.EventHandler(this.bt_Remove_Click);
+            // 
+            // pb_Picture1
+            // 
+            this.pb_Picture1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Picture1.Location = new System.Drawing.Point(48, 84);
+            this.pb_Picture1.Name = "pb_Picture1";
+            this.pb_Picture1.Size = new System.Drawing.Size(600, 388);
+            this.pb_Picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb_Picture1.TabIndex = 2;
+            this.pb_Picture1.TabStop = false;
+            // 
+            // ofd_picture
+            // 
+            this.ofd_picture.FileName = "openFileDialog1";
+            // 
+            // F_Picture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.pb_Picture1);
+            this.Controls.Add(this.bt_Remove);
+            this.Controls.Add(this.bt_select);
+            this.Name = "F_Picture";
+            this.Text = "Picture";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.F_Picture_FormClosed);
+            this.Load += new System.EventHandler(this.F_Picture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Picture1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_select;
+        private System.Windows.Forms.Button bt_Remove;
+        private System.Windows.Forms.PictureBox pb_Picture1;
+        private System.Windows.Forms.OpenFileDialog ofd_picture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using Pictures.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pictures
+{
+    public partial class F_Picture : Form
+    {
+        public string path
+        {
+            get
+            {
+                return Settings.Default.path;
+            }
+            set
+            {
+                Settings.Default.path = value;
+            }
+        }
+        public bool IsMaximize
+        {
+            get
+            {
+                return Settings.Default.IsMaximize;
+            }
+            set
+            {
+                Settings.Default.IsMaximize = value;
+            }
+        }
+
+        public F_Picture()
+        {
+            InitializeComponent();
+            pb_Picture1.ImageLocation = path;
+            if (path != null)
+               bt_Remove.Enabled = true;
+
+            if (IsMaximize)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void F_Picture_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_select_Click(object sender, EventArgs e)
+        {
+            ofd_picture.InitialDirectory = "c:\\";
+            ofd_picture.Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            ofd_picture.RestoreDirectory = true;
+            if (ofd_picture.ShowDialog() == DialogResult.OK)
+            {
+                pb_Picture1.ImageLocation = ofd_picture.FileName;
+                //path = ofd_picture.FileName;
+                //pb_Picture1.ImageLocation = path;
+                bt_Remove.Enabled = true;
+            }
+        }
+
+        private void bt_Remove_Click(object sender, EventArgs e)
+        {
+            pb_Picture1.Image = null;
+            bt_Remove.Enabled = false;
+            //path = null;
+            pb_Picture1.ImageLocation = null;
+        }
+
+        private void F_Picture_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                IsMaximize = true;
+            else
+                IsMaximize = false;
+
+                path = pb_Picture1.ImageLocation;
+
+            Settings.Default.Save();
+        }
+    }
+}
